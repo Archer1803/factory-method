d646aeef6c47c96c0fac8c32f457dfa921cf1ba2
Archer <nhuquynhthi2k4@gmail.com>
2023-11-24 21:05:19 +0700

    vi du

---
+++ thuvien/thuvien/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using thuvien;
+
+namespace Bai_lam
+{
+    interface Iquanly
+    {
+        public void loaisach();
+    }
+    class thuvien
+    {
+        static void Main(string[] args)
+        {
+            sach s = new sach();
+            Iquanly q = s.Creatsach(2);
+            q.loaisach();
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ thuvien/thuvien/sach.cs
@@ -0,0 +1,26 @@
+﻿using Bai_lam;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thuvien
+{
+    internal class sach
+    {
+        public Iquanly Creatsach(int so)
+        {
+
+            switch (so)
+                {
+                case 1:
+                    return new xahoi();
+                case 2:
+                    return new tunhien();
+                default:
+                    return null;
+                  }
+        }
+    }
+}
