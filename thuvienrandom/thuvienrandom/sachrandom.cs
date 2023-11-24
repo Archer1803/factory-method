cdaebc68701e0db1f9b07cfd6052a87b7dc0c284
Archer <nhuquynhthi2k4@gmail.com>
2023-11-25 00:35:56 +0700

    vi du random

---
+++ thuvienrandom/thuvienrandom/sachrandom.cs
@@ -0,0 +1,39 @@
+﻿using Bai_lam;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using thuvien;
+
+namespace thuvienrandom
+{
+    internal class sachrandom 
+    {
+        
+            private Random random;
+
+        public sachrandom()
+        {           
+            random = new Random();
+        }
+            public Iquanly Creatsach()
+            {           
+            int so = random.Next(1, 3);
+            Console.WriteLine(" so random la " + so);     
+            switch (so)
+                {
+                    case 1:
+                        return new xahoi();
+                    case 2:
+                        return new tunhien();
+                    default:
+                        return null;
+                }
+            }
+        }
+
+
+    }
+
+
---
+++ thuvienrandom/thuvienrandom/tunhien.cs
@@ -0,0 +1,17 @@
+﻿using Bai_lam;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thuvien
+{
+    internal class tunhien : Iquanly
+    {
+        public void loaisach()
+        {
+            Console.WriteLine(" Day la sach mon tu nhien ");
+        }
+    }
+}
